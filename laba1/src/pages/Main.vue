f9e88c84c737317c0f13b329a5af623009659a8c
Ivan Zaburdaev <ivanlom227@gmail.com>
2022-02-04 20:05:11 +0300

    Починись пожалуйста

--- laba1/src/pages/Main.vue
+++ laba1/src/pages/Main.vue
@@ -1,11 +1,45 @@
 <template>
   <h1>Добро пожаловать !</h1>
+  <div class="slider">
+    <h1>{{slides[current].name}}</h1>
+    <h2>{{slides[current].shortDesc}}</h2>
+  </div>
 </template>
 
 <script>
+
+import axios from 'axios';
 export default {
-  name: "Main"
+  name: "Main",
+  data(){
+    return{
+      current:0,
+      slides:[],
+    }
+  },
+  methods:{
+    async fetchPosts(){
+      try{
+          const response = await axios.get('/articles.json');
+          this.slides=response.data;
+
+      }catch (error){
+        alert('ERROR');
+      }
+    },
+    goNext(){
+      this.current+=1;
+    }
+  },
+  beforeMount() {
+    this.fetchPosts();
+  },
+  computed:{
+
+  }
+
 }
+
 </script>
 
 <style scoped>
